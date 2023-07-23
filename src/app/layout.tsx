fcaf98718ca4c8a872e9ffb2c1e450817fb726e1
lmw <alsdndjdrktl@gmail.com>
2023-07-23 11:49:26 +0900

    feat(#1): 반응형 헤더
    - NavBar 컴포넌트
    - 반응형

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import NavBar from '@/components/layout/navBar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -11,8 +12,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
  return (
-  <html lang="en">
-   <body className={inter.className}>{children}</body>
+  <html lang="ko">
+   <body className={inter.className}>
+    <NavBar />
+    {children}
+   </body>
   </html>
  )
 }
